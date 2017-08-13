bc088a658ddcf8831dbf7af6020a14989853f02b
jjallaire <jj@rstudio.com>
2017-08-12 21:40:35 -0400

    pass run_dir rather than runs_dir to training_run()

--- R/training_run.R
+++ R/training_run.R
@@ -5,13 +5,11 @@
 #'
 #' @inheritParams  flags
 #' @param file Path to training script (defaults to "train.R")
-#' @param runs_dir Directory to create run directories within
 #' @param type Run type (defaults to "local")
 #' @param flags Named character vector with flag values (see [flags()])
+#' @param run_dir Directory to store run data within
 #' @param echo Print expressions within training script
 #' @param envir The environment in which the script should be evaluated
-#' @param chdir If `TRUE` the R working directory is temporarily changed to the
-#'   directory containing file for evaluating.
 #' @param encoding The encoding of the training script; see [file()].
 #'
 #' @return The directory used for the training run.
@@ -21,60 +19,45 @@ training_run <- function(file = "train.R",
                          type = "local",
                          config = Sys.getenv("R_CONFIG_ACTIVE", unset = "default"),
                          flags = NULL,
-                         runs_dir = "runs",
+                         run_dir = NULL,
                          echo = FALSE,
                          envir = parent.frame(),
-                         chdir = TRUE,
                          encoding = getOption("encoding")) {
 
   # verify that the file exists
   if (!file.exists(file))
     stop("The specified R script '", file, "' does not exist.")
 
-  # chdir if requested
-  wd <- getwd()
-  if (chdir) {
-    on.exit(setwd(wd), add = TRUE)
-    setwd(dirname(file))
-    file <- basename(file)
-  }
-
   # setup run context
   run_dir <- initialize_run(
     type = type,
     config = config,
     flags = flags,
-    runs_dir = runs_dir
+    run_dir = run_dir
   )
   on.exit(clear_run(), add = TRUE)
 
-  # notify user of run dir (print full path if it's not relative to the invocation dir)
-  absolute_run_dir <- normalizePath(run_dir)
-  if (!identical(absolute_run_dir, normalizePath(file.path(wd, run_dir), mustWork = FALSE)))
-    run_dir <- gsub(Sys.getenv("HOME"), "~", absolute_run_dir)
+  # notify user of run dir
   message("Using run directory at ", run_dir)
 
   # perform the run
-  source(file = file,
-         local = envir,
-         echo = echo,
-         chdir = chdir,
-         encoding = encoding)
+  source(file = file, local = envir, echo = echo, encoding = encoding)
 
   # return the run_dir
-  invisible(absolute_run_dir)
+  invisible(run_dir)
 }
 
 initialize_run <- function(type = "local",
                            config = Sys.getenv("R_CONFIG_ACTIVE", unset = "default"),
                            flags = NULL,
-                           runs_dir = "runs") {
+                           run_dir = NULL) {
 
   # clear any existing run
   clear_run()
 
   # generate the run_dir
-  run_dir <- unique_dir(runs_dir, format = "%Y-%m-%dT%H-%M-%SZ")
+  if (is.null(run_dir))
+    run_dir <- unique_dir("runs", format = "%Y-%m-%dT%H-%M-%SZ")
 
   # create the directory if necessary
   if (!utils::file_test("-d", run_dir))
@@ -100,7 +83,6 @@ initialize_run <- function(type = "local",
 
   # return invisibly
   invisible(run_dir)
-
 }
 
 clear_run <- function() {
