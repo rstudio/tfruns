b0ef1f3bdacef2ba0eeb29f045bdb084f3f43681
jjallaire <jj@rstudio.com>
2017-08-15 10:14:08 -0400

    initial work on run_info

---
+++ R/run_info.R
@@ -0,0 +1,22 @@
+
+
+
+
+#' @export
+run_info <- function(run_dir) {
+  return_runs(run_record(run_dir))
+}
+
+#' @rdname run_info
+#' @export
+run_metrics <- function(run_dir) {
+
+
+}
+
+#' @rdname run_info
+#' @export
+run_source <- function(run_dir) {
+
+}
+
