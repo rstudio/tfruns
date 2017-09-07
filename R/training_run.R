ff232f52b9460ea6f6ca332c3a946902c01aa9ab
jjallaire <jj@rstudio.com>
2017-09-07 14:25:29 -0400

    move view_training_run function

--- R/training_run.R
+++ R/training_run.R
@@ -136,3 +136,14 @@ clear_run <- function() {
 }
 
 
+view_training_run <- function() {
+  viewer_dir <- tempfile("view-training-run")
+  dir.create(viewer_dir)
+  viewer_html <- file.path(viewer_dir, "index.html")
+  render_view("training_run", viewer_html)
+  #getOption("page_viewer")(viewer_html)
+  browser_viewer(viewer_dir)(viewer_html)
+}
+
+
+
