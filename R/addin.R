e775fc7a48b7a9b26f29a1d108ef74f123df7d5c
JJ Allaire <jj@rstudio.com>
2017-09-23 07:20:00 -0400

    save all before training_run

--- R/addin.R
+++ R/addin.R
@@ -3,6 +3,7 @@ add_in_training_run <- function() {
   editor_context <- rstudioapi::getSourceEditorContext()
   editor_path <- editor_context$path
   if (isTRUE(nzchar(editor_path))) {
+    rstudioapi::documentSaveAll()
     normalized_wd <- normalizePath(getwd(), winslash = "/", mustWork = FALSE)
     normalized_editor_path <- normalizePath(editor_path, winslash = "/", mustWork = FALSE)
     if (grepl(paste0("^", normalized_wd), normalized_editor_path))
