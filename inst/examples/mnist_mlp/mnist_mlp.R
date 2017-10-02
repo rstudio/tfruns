84d01337912305940140e47cc76dd7cd3e3da2c2
jjallaire <jj@rstudio.com>
2017-10-02 10:07:49 -0400

    add mnist_mlp.R example

--- inst/examples/mnist_mlp/mnist_mlp.R
+++ inst/examples/mnist_mlp/mnist_mlp.R
@@ -6,11 +6,14 @@
 
 library(keras)
 
-# Data Preparation ---------------------------------------------------
+# Hyperparameter flags ---------------------------------------------------
 
-batch_size <- 128
-num_classes <- 10
-epochs <- 20
+FLAGS <- flags(
+  flag_numeric("dropout1", 0.4),
+  flag_numeric("dropout2", 0.3)
+)
+
+# Data Preparation ---------------------------------------------------
 
 # The data, shuffled and split between train and test sets
 mnist <- dataset_mnist()
@@ -28,22 +31,22 @@ x_train <- x_train / 255
 x_test <- x_test / 255
 
 # Convert class vectors to binary class matrices
-y_train <- to_categorical(y_train, num_classes)
-y_test <- to_categorical(y_test, num_classes)
+y_train <- to_categorical(y_train, 10)
+y_test <- to_categorical(y_test, 10)
 
 # Define Model --------------------------------------------------------------
 
 model <- keras_model_sequential()
 model %>%
   layer_dense(units = 256, activation = 'relu', input_shape = c(784)) %>%
-  layer_dropout(rate = 0.4) %>%
+  layer_dropout(rate = FLAGS$dropout1) %>%
   layer_dense(units = 128, activation = 'relu') %>%
-  layer_dropout(rate = 0.3) %>%
+  layer_dropout(rate = FLAGS$dropout2) %>%
   layer_dense(units = 10, activation = 'softmax')
 
 model %>% compile(
   loss = 'categorical_crossentropy',
-  optimizer = optimizer_rmsprop(),
+  optimizer = optimizer_rmsprop(lr = 0.001),
   metrics = c('accuracy')
 )
 
@@ -51,8 +54,8 @@ model %>% compile(
 
 history <- model %>% fit(
   x_train, y_train,
-  batch_size = batch_size,
-  epochs = epochs,
+  batch_size = 128,
+  epochs = 20,
   verbose = 1,
   validation_split = 0.2
 )
