fffbd5f8ec3dfa4853c162494f7a5efe01ff80b0
Daniel Falbel <dfalbel@gmail.com>
2019-08-26 09:22:05 -0300

    stop reseting default graph when executing eagerly

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,18 @@
 is_windows <- function() {
   identical(.Platform$OS.type, "windows")
 }
+
+# we cant use tensorflow::tf_version() to not create a
+# dependency
+tf_version <- function(tf) {
+
+  if (reticulate::py_has_attr(tf, "version"))
+    version_raw <- tf$version$VERSION
+  else version_raw <- tf$VERSION
+
+  tfv <- strsplit(version_raw, ".", fixed = TRUE)[[1]]
+  version <- package_version(paste(tfv[[1]], tfv[[2]],
+                                   sep = "."))
+
+  version
+}
